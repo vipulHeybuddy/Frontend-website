9aa8e836cf0a1f6a92059ac97dc3d4216772be13
shruti <shruti250902@gmail.com>
2024-02-01 17:57:58 +0530

    3d

--- src/components/servicescomp/3dmodelingcomp/Section8.jsx
+++ src/components/servicescomp/3dmodelingcomp/Section8.jsx
@@ -420,20 +420,7 @@ const Pagenation = () => {
 
           </div>
         </TabPanel> */}
-        <div style={textContainerStyle}>
-          <h1 className="py-4 text-2xl lg:text-3xl">
-            Creative, Customised, and Cost-effective VR development software
-            services with Hey Budy.
-          </h1>
-          <Button
-            style={buttonHeader}
-            onMouseEnter={() => setIsHovered(true)}
-            onMouseLeave={() => setIsHovered(false)}
-            className="text-2xl"
-          >
-            Discuss Your Project
-          </Button>
-        </div>
+        
 
       </div>
     </motion.div>
